f0da3c2a06830a51a8e99827ae95d6936bb50e3b
Dombah <domagoj.leskovic24@gmail.com>
2025-02-16 13:56:13 +0100

    Inital commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HeartHealthPortal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HeartHealthPortal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
